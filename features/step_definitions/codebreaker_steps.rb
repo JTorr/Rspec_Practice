bb98c7179f42fd3631c3667b20e4583c0786fbe8
Julie Torres <reeddetorres@gmail.com>
2014-11-09 08:07:09 -0500

    Set up Rspec test environment

--- features/step_definitions/codebreaker_steps.rb
+++ features/step_definitions/codebreaker_steps.rb
@@ -1,18 +1,39 @@
+class Output
+  def messages
+    @messages ||= []
+  end
+
+  def puts(message)
+    messages << message
+  end
+end
+
+def output
+  output ||= Output.new
+end
+
+
+
 Given(/^I am not yet playing$/) do
 end
 
 When(/^I start a new game$/) do
-  CodeBreaker::Game.new.start
+  game = CodeBreaker::Game.new(output)
+  game.start
 end
 
-Given(/^the secret code is "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then(/^I should see "([^"]*)"$/) do |message|
+  expect(output.messages).to include(message)
 end
 
-When(/^I guess "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^the mark should be "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
+# Given(/^the secret code is "(.*?)"$/) do |arg1|
+#   pending # express the regexp above with the code you wish you had
+# end
+#
+# When(/^I guess "(.*?)"$/) do |arg1|
+#   pending # express the regexp above with the code you wish you had
+# end
+#
+# Then(/^the mark should be "(.*?)"$/) do |arg1|
+#   pending # express the regexp above with the code you wish you had
+# end
