25ba42f0858bd1f1b69e86e019b1ce5b2491b4ab
Julie Torres <reeddetorres@gmail.com>
2014-11-09 08:14:19 -0500

    First test for codebreaker RED

--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -3,7 +3,15 @@ require 'spec_helper'
 module CodeBreaker
   describe Game do
     describe "#start" do
-      it "sends a welcome message"
+      it "sends a welcome message" do
+        output = double('output')
+        game = Game.new(output)
+
+        expect(output).to receive(:puts).with("Welcome to Codebreaker!")
+
+        game.start
+      end
+      
       it "prompts for the first guess"
     end
   end
